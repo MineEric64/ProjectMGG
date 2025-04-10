c392a9d1c7cf626f4caa71babb49d3758408fc92
MineEric64 <besteric40@gmail.com>
2025-04-10 21:52:09 +0900

    close #23

--- Assets/Scripts/Ingame/Script/Keywords/Essential/GetVariable.cs
+++ Assets/Scripts/Ingame/Script/Keywords/Essential/GetVariable.cs
@@ -13,14 +13,16 @@ namespace ProjectMGG.Ingame.Script.Keywords
         public object Interpret()
         {
             var glocal1 = IngameManagerV2.GetVariable(Name, ref IngameManagerV2.Local.Others, ref IngameManagerV2.Global.Others);
-            var glocal2 = IngameManagerV2.GetVariable(Name, ref IngameManagerV2.Local.Transforms, ref IngameManagerV2.Global.Transforms);
-            var glocal3 = IngameManagerV2.GetVariable(Name, ref IngameManagerV2.Local.Characters, ref IngameManagerV2.Global.Characters);
-            var glocal4 = IngameManagerV2.GetVariable(Name, ref IngameManagerV2.Local.Images, ref IngameManagerV2.Global.Images);
-
             if (glocal1 != null) return glocal1;
-            else if (glocal2 != null) return glocal2;
-            else if (glocal3 != null) return glocal3;
-            else if (glocal4 != null) return glocal4;
+
+            var glocal2 = IngameManagerV2.GetVariable(Name, ref IngameManagerV2.Local.Transforms, ref IngameManagerV2.Global.Transforms);
+            if (glocal2 != null) return glocal2;
+
+            var glocal3 = IngameManagerV2.GetVariable(Name, ref IngameManagerV2.Local.Characters, ref IngameManagerV2.Global.Characters);
+            if (glocal3 != null) return glocal3;
+
+            var glocal4 = IngameManagerV2.GetVariable(Name, ref IngameManagerV2.Local.Images, ref IngameManagerV2.Global.Images);
+            if (glocal4 != null) return glocal4;
 
             if (Interpreter.FunctionTable.ContainsKey(Name))
             {
